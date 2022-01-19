392586b64b2495f8f30bfcf5fda62123e5edee7b
thinkgo <thinkgo@aliyun.com>
2022-01-19 19:48:10 +0800

    upgrade package use go 1.17.x add more coverage

--- requestid_test.go
+++ requestid_test.go
@@ -1,7 +1,6 @@
 package requestid
 
 import (
-	"context"
 	"fmt"
 	"net/http"
 	"net/http/httptest"
@@ -9,6 +8,10 @@ import (
 
 	"github.com/gin-gonic/gin"
 )
+
+func init() {
+	gin.SetMode(gin.ReleaseMode)
+}
 
 func TestRequestID(t *testing.T) {
 	tests := map[string]struct {
@@ -19,22 +22,22 @@ func TestRequestID(t *testing.T) {
 		"Retrieves Request Id from default header": {
 			"X-Request-Id",
 			func() *http.Request {
-				req, _ := http.NewRequestWithContext(context.TODO(), "GET", "/", nil)
+				req := httptest.NewRequest("GET", "/", nil)
 				req.Header.Add("X-Request-Id", "req-123456")
 
 				return req
 			},
-			"RequestID: req-123456",
+			"RequestId: req-123456",
 		},
 		"Retrieves Request Id from custom header": {
 			"X-Trace-Id",
 			func() *http.Request {
-				req, _ := http.NewRequestWithContext(context.Background(), "GET", "/", nil)
+				req := httptest.NewRequest("GET", "/", nil)
 				req.Header.Add("X-Trace-Id", "trace:abc123")
 
 				return req
 			},
-			"RequestID: trace:abc123",
+			"RequestId: trace:abc123",
 		},
 	}
 
@@ -46,13 +49,34 @@ func TestRequestID(t *testing.T) {
 		r.Use(RequestID(WithRequestIDHeader(test.requestIDHeader)))
 		r.GET("/", func(c *gin.Context) {
 			requestID := GetRequestID(c)
-			response := fmt.Sprintf("RequestID: %s", requestID)
+			response := fmt.Sprintf("RequestId: %s", requestID)
 			_, _ = w.WriteString(response)
 		})
 		r.ServeHTTP(w, test.request())
 
 		if w.Body.String() != test.expectedResponse {
-			t.Fatalf("RequestID was not the expected value")
+			t.Fatalf("RequestId was not the expected value")
 		}
+	}
+}
+
+func TestFirstRequest(t *testing.T) {
+	var gotResponse string
+
+	w := httptest.NewRecorder()
+
+	r := gin.New()
+	r.Use(RequestID(WithNextRequestID(NextRequestID)))
+	r.GET("/", func(c *gin.Context) {
+		requestId := GetRequestID(c)
+		gotResponse = fmt.Sprintf("RequestId: %s", requestId)
+		_, _ = w.WriteString(gotResponse)
+	})
+
+	req := httptest.NewRequest("GET", "/", nil)
+	r.ServeHTTP(w, req)
+
+	if w.Body.String() != gotResponse {
+		t.Fatalf("RequestId was not the expected value")
 	}
 }
